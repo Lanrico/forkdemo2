a7c762b5b41caed6bcc9b3946d98621ad9bcc59c
Guanlan Ji <228298909@qq.com>
2022-11-08 10:15:40 +0000

    checkReorder feature added.

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -27,5 +27,13 @@ class Catalogue {
     return removedProduct;
   }
 
+  checkReorders() {
+    const result = { type: "Reorder", productIds: [] };
+    result.productIds = this.products
+      .filter((p) => p.quantityInStock <= p.reorderLevel)
+      .map((p) => p.id);
+    return result;
+  }
+
 }
 module.exports = Catalogue;
--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -37,11 +37,27 @@ describe("Catalogue", () => {
       expect(result).to.be.undefined;
     });
   });
-
-
-
-
-
-
-
+  describe("checkReorder", () => {
+    it("should return an empty array when no products need reordering", function () {
+      const result = cat.checkReorders();
+      expect(result.productIds).to.be.empty;
+    });
+    it("should report products that satisfy reorder criteria", function () {
+      cat.addProduct(new Product("B123", "Product 4", 10, 20, 10.0));
+      cat.addProduct(new Product("B124", "Product 5", 10, 30, 10.0));
+      const result = cat.checkReorders();
+      expect(result.productIds).to.have.lengthOf(2);
+      expect(result.productIds).to.have.members(["B123", "B124"]);
+    });
+    it("should include products just on their reorder level", function () {
+      cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
+      const result = cat.checkReorders();
+      expect(result.productIds).to.have.members(["B125"]);
+    });
+    it("should handle an empty catalogue", function () {
+      cat = new Catalogue("Test catalogue");
+      const result = cat.checkReorders();
+      expect(result.productIds).to.be.empty;
+    });
+  });
 });
